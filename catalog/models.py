018ee1e3d93ee4fc362bbb196174a65debaa523f
akhmad-john <akhmad_shams@mail.ru>
2020-10-09 10:16:59 +0500

    check commit

--- catalog/models.py
+++ catalog/models.py
@@ -26,6 +26,7 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+
 class ProductParameter(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     name = models.CharField(max_length=40)
